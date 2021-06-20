a252972d5da9a3c71115f600c41cfbe7fb2af208
nkvar12 <nkvar12@gmail.com>
2021-06-20 13:23:18 +0300

    added rabbitmq configuration - updated common lib version to 0.0.6 - added spring.rabbitmq.template.exchange property in bootstrap.properties - added RabbitMQConfig import in EntityServiceApplication

--- src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
+++ src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.universe.simulator.eventservice;
 
+import com.example.universe.simulator.common.config.RabbitMQConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(RabbitMQConfig.class)
 public class EventServiceApplication {
 
     public static void main(String[] args) {
