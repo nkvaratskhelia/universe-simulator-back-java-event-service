77a10fb0033443c644c47700bacb1ceac0169cbf
nkvaratskhelia <nkvar12@gmail.com>
2022-06-23 22:24:13 +0400

    added rabbitmq properties - added rabbitmq props in application.properties - removed rabbitmq & logstash props in application-test.properties - added RabbitMQProperties in EventServiceApplication - added rabbitmq props in properties section in project-setup.adoc - updated common lib to 1.3.2

--- src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
+++ src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
@@ -2,13 +2,14 @@ package com.example.universe.simulator.eventservice;
 
 import com.example.universe.simulator.common.config.CommonProperties;
 import com.example.universe.simulator.common.config.RabbitMQConfig;
+import com.example.universe.simulator.common.config.RabbitMQProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CommonProperties.class)
+@EnableConfigurationProperties({CommonProperties.class, RabbitMQProperties.class})
 @Import(RabbitMQConfig.class)
 public class EventServiceApplication {
 
