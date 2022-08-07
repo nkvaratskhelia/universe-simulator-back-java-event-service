0d3555ec1cbabd928395702bb3730b73375dbbf5
ikagotso <ikagotso@gmail.com>
2022-08-07 14:42:57 +0400

    removed liquibase, updated project docs, added mongo testcontainers.

--- src/main/java/com/example/universe/simulator/eventservice/services/EventService.java
+++ src/main/java/com/example/universe/simulator/eventservice/services/EventService.java
@@ -4,12 +4,10 @@ import com.example.universe.simulator.eventservice.entities.Event;
 import com.example.universe.simulator.eventservice.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class EventService {
 
@@ -19,7 +17,6 @@ public class EventService {
         return repository.findAll();
     }
 
-    @Transactional
     public Mono<Event> add(Event entity) {
         return repository.findByTypeAndDataAndTime(entity.type(), entity.data(), entity.time())
             .switchIfEmpty(Mono.defer(() -> repository.save(entity)));
--- src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 
 @AbstractSpringBootTest
@@ -14,17 +14,17 @@ import org.testcontainers.containers.RabbitMQContainer;
 abstract class AbstractIntegrationTest {
 
     private static final RabbitMQContainer RABBITMQ_CONTAINER;
-    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
+    private static final MongoDBContainer MONGODB_CONTAINER;
 
     @Autowired
     protected WebTestClient webClient;
 
     static {
         RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.10.6-management");
-        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:14.4");
+        MONGODB_CONTAINER = new MongoDBContainer("mongo:5.0.9");
 
         RABBITMQ_CONTAINER.start();
-        POSTGRESQL_CONTAINER.start();
+        MONGODB_CONTAINER.start();
     }
 
     @DynamicPropertySource
@@ -34,13 +34,6 @@ abstract class AbstractIntegrationTest {
         registry.add("spring.rabbitmq.username", RABBITMQ_CONTAINER::getAdminUsername);
         registry.add("spring.rabbitmq.password", RABBITMQ_CONTAINER::getAdminPassword);
 
-        registry.add("spring.r2dbc.url", () -> POSTGRESQL_CONTAINER.getJdbcUrl().replace("jdbc", "r2dbc"));
-        registry.add("spring.r2dbc.username", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.r2dbc.password", POSTGRESQL_CONTAINER::getPassword);
-
-        // needed because liquibase doesn't support r2dbc yet
-        registry.add("spring.liquibase.url", POSTGRESQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.liquibase.user", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.liquibase.password", POSTGRESQL_CONTAINER::getPassword);
+        registry.add("spring.data.mongodb.uri", MONGODB_CONTAINER::getReplicaSetUrl);
     }
 }
