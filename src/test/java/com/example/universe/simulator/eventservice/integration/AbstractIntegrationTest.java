3894262fc1fb348fa9868e6598a7e62bc2975216
nkvaratskhelia <nkvar12@gmail.com>
2023-01-07 22:05:56 +0400

    updated dependencycheck plugin - updated dependencycheck plugin to 7.4.4 - updated springBootAdminVersion ton 2.7.10 - updated rabbitmq to 3.11.6 in AbstractIntegrationTest

--- src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
@@ -20,7 +20,7 @@ abstract class AbstractIntegrationTest {
     protected WebTestClient webClient;
 
     static {
-        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.5-management");
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.6-management");
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:15.1");
 
         RABBITMQ_CONTAINER.start();
