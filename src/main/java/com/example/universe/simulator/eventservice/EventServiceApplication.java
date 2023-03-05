3ee5bd1b30566b563f1df1b95b67712b889b0449
nkvaratskhelia <nkvar12@gmail.com>
2023-03-05 15:05:59 +0400

    updated common - removed RabbitMQProperties in EnableConfigurationProperties in EventServiceApplication - updated common version to 1.9.5

--- src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
+++ src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
@@ -2,14 +2,13 @@ package com.example.universe.simulator.eventservice;
 
 import com.example.universe.simulator.common.config.CommonProperties;
 import com.example.universe.simulator.common.config.RabbitMQConfig;
-import com.example.universe.simulator.common.config.RabbitMQProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableConfigurationProperties({CommonProperties.class, RabbitMQProperties.class})
+@EnableConfigurationProperties(CommonProperties.class)
 @Import(RabbitMQConfig.class)
 public class EventServiceApplication {
 
