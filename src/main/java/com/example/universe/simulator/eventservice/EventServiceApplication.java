1d23657625998a9b8898a3fdf1a4d41d49b1ca8d
nkvaratskhelia <nkvar12@gmail.com>
2023-03-05 15:29:38 +0400

    removed @EnableConfigurationProperties(CommonProperties.class) in main class

--- src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
+++ src/main/java/com/example/universe/simulator/eventservice/EventServiceApplication.java
@@ -1,14 +1,11 @@
 package com.example.universe.simulator.eventservice;
 
-import com.example.universe.simulator.common.config.CommonProperties;
 import com.example.universe.simulator.common.config.RabbitMQConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CommonProperties.class)
 @Import(RabbitMQConfig.class)
 public class EventServiceApplication {
 
