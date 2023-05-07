0ac3b99ae49685416431376a36fb6aa6a6966ef0
nkvaratskhelia <nkvar12@gmail.com>
2023-05-07 15:50:05 +0400

    updated rabbitmq - updated rabbitmq to 3.11.15 in AbstractIntegrationTest - updated spring boot admin to 3.0.3

--- src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
@@ -21,7 +21,7 @@ abstract class AbstractIntegrationTest {
     protected WebTestClient webClient;
 
     static {
-        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.13-management");
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.15-management");
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:15.2");
 
         Startables.deepStart(RABBITMQ_CONTAINER, POSTGRESQL_CONTAINER).join();
