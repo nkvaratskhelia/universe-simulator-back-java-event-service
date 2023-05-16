90252668489922e697affb2caaa37f50ceedc706
nkvaratskhelia <nkvar12@gmail.com>
2023-05-16 07:24:46 +0400

    updated rabbitmq - updated rabbitmq to 3.11.16 in AbstractIntegrationTest - updated common version to 2.1.1

--- src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
@@ -21,7 +21,7 @@ abstract class AbstractIntegrationTest {
     protected WebTestClient webClient;
 
     static {
-        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.15-management");
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.11.16-management");
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:15.3");
 
         Startables.deepStart(RABBITMQ_CONTAINER, POSTGRESQL_CONTAINER).join();
