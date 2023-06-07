e03641b27fa222b0091845c05885468e40c1049a
nkvaratskhelia <nkvar12@gmail.com>
2023-06-07 11:34:43 +0400

    added spring-boot-testcontainers support - added spring-boot-testcontainers dependency - added org.testcontainers:r2dbc dependency - replaced @DynamicPropertySource with @ServiceConnection-s in AbstractIntegrationTest

--- src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
@@ -3,8 +3,7 @@ package com.example.universe.simulator.eventservice.integration;
 import com.example.universe.simulator.common.test.AbstractSpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
@@ -14,7 +13,10 @@ import org.testcontainers.lifecycle.Startables;
 @AutoConfigureWebTestClient
 abstract class AbstractIntegrationTest {
 
+    @ServiceConnection
     private static final RabbitMQContainer RABBITMQ_CONTAINER;
+
+    @ServiceConnection
     private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
     @Autowired
@@ -25,22 +27,5 @@ abstract class AbstractIntegrationTest {
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:15.3");
 
         Startables.deepStart(RABBITMQ_CONTAINER, POSTGRESQL_CONTAINER).join();
-    }
-
-    @DynamicPropertySource
-    private static void addTestContainersProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.rabbitmq.host", RABBITMQ_CONTAINER::getHost);
-        registry.add("spring.rabbitmq.port", RABBITMQ_CONTAINER::getAmqpPort);
-        registry.add("spring.rabbitmq.username", RABBITMQ_CONTAINER::getAdminUsername);
-        registry.add("spring.rabbitmq.password", RABBITMQ_CONTAINER::getAdminPassword);
-
-        registry.add("spring.r2dbc.url", () -> POSTGRESQL_CONTAINER.getJdbcUrl().replace("jdbc", "r2dbc"));
-        registry.add("spring.r2dbc.username", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.r2dbc.password", POSTGRESQL_CONTAINER::getPassword);
-
-        // needed because liquibase doesn't support r2dbc yet
-        registry.add("spring.liquibase.url", POSTGRESQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.liquibase.user", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.liquibase.password", POSTGRESQL_CONTAINER::getPassword);
     }
 }
