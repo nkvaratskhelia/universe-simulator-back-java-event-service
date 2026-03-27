824f2e483bc35b56cf3b075bac5c151e1aceba48
nkvaratskhelia <nkvar12@gmail.com>
2026-03-27 13:05:13 +0400

    updates

--- src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/universe/simulator/eventservice/integration/AbstractIntegrationTest.java
@@ -23,7 +23,7 @@ abstract class AbstractIntegrationTest {
     protected WebTestClient webClient;
 
     static {
-        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:4.2.4-management");
+        RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:4.2.5-management");
         POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:18.3");
 
         Startables.deepStart(RABBITMQ_CONTAINER, POSTGRESQL_CONTAINER).join();
